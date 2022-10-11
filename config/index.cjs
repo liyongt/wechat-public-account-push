0182504e5fe72a790b2736a6d780652d2b34a8d3
liyongt <114457249+liyongt@users.noreply.github.com>
2022-10-11 20:56:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -52,7 +52,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'R_Z39gkgllcC13z3zXioI4AlKUyiBvMCOzHv3dsrnoE',
+  CALLBACK_TEMPLATE_ID: 'wVbgXzecIuOKTrGOxWtFQiFxChLEH1ZO51K8ZOX9M4w',
 
   CALLBACK_USERS: [
     {
