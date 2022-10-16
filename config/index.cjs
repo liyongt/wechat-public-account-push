0e4d3590486aca16342ee3d75cec16794ad9c5ef
liyongt <114457249+liyongt@users.noreply.github.com>
2022-10-16 19:10:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU16812TArLi6412LXSrlzHVglwsc046mGQxKRtN',
+      id: 'PDU16812TArLi6412LXSrlzHVglwsc046mGQxKRtN,PDU16801TFRjwM3BRpxRgJFi9NO2MdHOvpjQIW9mJ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'wVbgXzecIuOKTrGOxWtFQiFxChLEH1ZO51K8ZOX9M4w',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
